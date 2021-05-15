f76a9d77a9951a83e81b47cd2d512740d45ede82
Daniel Gatis <danielgatis@gmail.com>
2021-05-15 15:00:44 -0300

    add rmi step

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 git clone git@github-dgc.com:klyff/rakonto-backend.git 2> /dev/null || (cd rakonto-backend ; git pull)
 git clone git@github-dgc.com:klyff/rakonto-ui.git 2> /dev/null || (cd rakonto-ui ; git pull)
 
+docker rmi --force $(docker images 'rakonto-deploy-qa_rakonto' -a -q)
 docker-compose up --remove-orphans
