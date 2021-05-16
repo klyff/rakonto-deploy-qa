17d6ad09285f47fbebbd34b938f5f848468e6a5c
Daniel Gatis <danielgatis@gmail.com>
2021-05-15 21:19:21 -0300

    fix github repo

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-git clone git@github-dgc.com:klyff/rakonto-backend.git 2> /dev/null || (cd rakonto-backend ; git pull)
-git clone git@github-dgc.com:klyff/rakonto-ui.git 2> /dev/null || (cd rakonto-ui ; git pull)
+git clone git@github.com:klyff/rakonto-backend.git 2> /dev/null || (cd rakonto-backend ; git pull)
+git clone git@github.com:klyff/rakonto-ui.git 2> /dev/null || (cd rakonto-ui ; git pull)
 
 docker rmi --force $(docker images 'rakonto-deploy-qa_rakonto' -a -q)
 docker-compose up --remove-orphans
