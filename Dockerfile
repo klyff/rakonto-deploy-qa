fa7050a943c79d1fa159d8c5d9791587f5091da2
Daniel Gatis <danielgatis@gmail.com>
2021-05-22 00:40:40 -0300

    qa properties

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,17 @@
-FROM node as ui
+FROM node:16-alpine as ui
 WORKDIR /app
 COPY rakonto-ui .
 RUN yarn install && yarn build
 
-FROM openjdk:14-jdk-alpine as backend
+FROM openjdk:14-jdk-slim as backend
 WORKDIR /app
 COPY rakonto-backend .
 RUN mkdir -p src/main/resources/public
 COPY --from=ui /app/build src/main/resources/public
 RUN ./gradlew build
 
-FROM openjdk:14-jdk-alpine
+FROM openjdk:14-jdk-slim
+RUN apt-get update && apt-get install -y --no-install-recommends ffmpeg
 COPY --from=backend /app/build/libs/*.jar ./app.jar
 EXPOSE 8080
 ENV ENV=qa
--- run.sh
+++ run.sh
@@ -3,5 +3,23 @@
 git clone git@github.com:klyff/rakonto-backend.git 2> /dev/null || (cd rakonto-backend ; git pull)
 git clone git@github.com:klyff/rakonto-ui.git 2> /dev/null || (cd rakonto-ui ; git pull)
 
-docker rmi --force $(docker images 'rakonto-deploy-qa_rakonto' -a -q)
+if [[ ! -z $DROP_DATA ]]; then
+  echo removing ~/docker-data
+  rm -fr ~/rakonto-data
+fi
+
+cp rakonto-backend/docker-compose.yml . && cp -R rakonto-backend/docker .
+
+cat <<EOT >> docker-compose.yml
+
+  rakonto:
+    build: .
+    ports:
+      - 8080:8080
+    depends_on:
+      - mysql
+      - rabbitmq
+EOT
+
+docker rmi --force $(docker images 'rakonto-deploy-qa_rakonto' -a -q) 2> /dev/null
 docker-compose up --remove-orphans
