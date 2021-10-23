c9d27dd6dd0d60e3f03257f0924c6048439e5118
Daniel Gatis <danielgatis@gmail.com>
2021-10-23 00:45:37 -0300

    fix deploy

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-ENV="${ENV:-qa}"
-
 git clone git@github.com:klyff/rakonto-backend.git 2> /dev/null || (cd rakonto-backend ; git pull)
-git clone git@github.com:klyff/rakonto-ui.git 2> /dev/null || (cd rakonto-ui ; git pull)
 git clone git@github.com:klyff/rakonto-front.git 2> /dev/null || (cd rakonto-front ; git pull)
 
 if [[ ! -z $DROP_DATA ]]; then
@@ -11,31 +8,6 @@ if [[ ! -z $DROP_DATA ]]; then
   rm -fr ~/rakonto-data
 fi
 
-cp rakonto-backend/docker-compose.yml . && cp -R rakonto-backend/docker .
-
-cat <<EOT >> docker-compose.yml
-
-  rakonto:
-    build: .
-    ports:
-      - 8080:8080
-    volumes:
-      - ~/rakonto-data/docker/binary:/app/uploads
-    depends_on:
-      - mysql
-      - rabbitmq
-    environment:
-      ENV: ${ENV}
-  rakonto-ui:
-    build:
-      context: .
-      dockerfile: UI-Dockerfile
-    ports:
-      - 80:80
-    depends_on:
-      - rakonto
-
-EOT
-
-docker rmi --force $(docker images 'rakonto-deploy-qa_rakonto' -a -q) 2> /dev/null
-docker-compose up --remove-orphans
+# docker-compose -f ./rakonto-backend/docker-compose.yml -f ./docker-compose.yml build --no-cache backend
+# docker-compose -f ./rakonto-backend/docker-compose.yml -f ./docker-compose.yml build --no-cache frontend
+docker-compose -f ./rakonto-backend/docker-compose.yml -f ./docker-compose.yml up --remove-orphans
