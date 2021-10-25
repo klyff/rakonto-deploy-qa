0c8427b6db93b41615b334c26bb453dbedf50760
Daniel Gatis <danielgatis@gmail.com>
2021-10-24 21:27:38 -0300

    fix

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 git clone git@github.com:klyff/rakonto-backend.git 2> /dev/null || (cd rakonto-backend ; git pull)
-git clone git@github.com:klyff/rakonto-front.git 2> /dev/null || (cd rakonto-front ; git pull)
+git clone git@github.com:klyff/rakonto-ui.git 2> /dev/null || (cd rakonto-ui ; git pull)
 
 if [[ ! -z $DROP_DATA ]]; then
   echo removing ~/docker-data
   rm -fr ~/rakonto-data
 fi
 
-# docker-compose -f ./rakonto-backend/docker-compose.yml -f ./docker-compose.yml build --no-cache backend
-# docker-compose -f ./rakonto-backend/docker-compose.yml -f ./docker-compose.yml build --no-cache frontend
+docker-compose -f ./rakonto-backend/docker-compose.yml -f ./docker-compose.yml build backend
+docker-compose -f ./rakonto-backend/docker-compose.yml -f ./docker-compose.yml build frontend
 docker-compose -f ./rakonto-backend/docker-compose.yml -f ./docker-compose.yml up --remove-orphans
