a0e88d11c0f7acbbc6cad947b936139b79406082
Daniel Gatis <danielgatis@gmail.com>
2021-11-03 16:40:15 -0300

    fix

--- run.sh
+++ run.sh
@@ -3,10 +3,7 @@
 git clone git@github.com:klyff/rakonto-backend.git 2> /dev/null || (cd rakonto-backend ; git pull)
 git clone git@github.com:klyff/rakonto-ui.git 2> /dev/null || (cd rakonto-ui ; git pull)
 
-if [[ ! -z $DROP_DATA ]]; then
-  echo removing ~/docker-data
-  rm -fr ~/rakonto-data
-fi
+docker container kill $(docker ps -q) 2> /dev/null
 
 docker-compose -f ./rakonto-backend/docker-compose.yml -f ./docker-compose.yml build backend
 docker-compose -f ./rakonto-backend/docker-compose.yml -f ./docker-compose.yml build frontend
